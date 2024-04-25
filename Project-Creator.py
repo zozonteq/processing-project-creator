adc304fa7c1aaeebfbdfbe1ff86c4fd3316f666b
Oto Takahashi <ottotak@outlook.com>
2024-04-25 14:46:08 +0900

    ♻️ リファクタリング・コメントの追加

--- Project-Creator.py
+++ Project-Creator.py
@@ -8,7 +8,7 @@ TEMPLATE_PROJECT_NAME = "sketch_TEMPLATE"
 TEMPLATE_PROJECT_DIR = f"{DIR}/{TEMPLATE_PROJECT_NAME}"
 
 
-def start_new_project():
+def start_new_project(): # 新しくプロジェクトを作成する。
     PROJECT_NAME = "sketch_" + str(input("\nEnter your new project name :").replace(" ","_").replace("-","_"))
     PROJECT_DIR = f"{DIR}/{PROJECT_NAME}"
 
@@ -23,20 +23,22 @@ def start_new_project():
             f"{PROJECT_DIR}/{PROJECT_NAME}.pde")
         print("Project successfly created!")
     print("Finished")
+def create_template():  #Processingプロジェクトファイルのテンプレートを作成。
+    print(f"Project template file({TEMPLATE_PROJECT_NAME}) does not found.\nCreating project template file at {TEMPLATE_PROJECT_DIR}...")
+    os.makedirs(f"{TEMPLATE_PROJECT_DIR}",exist_ok=True)
+    program_file = pathlib.Path(f"{TEMPLATE_PROJECT_DIR}/{TEMPLATE_PROJECT_NAME}.pde")
+    program_file.touch()
+    print(f"Project template file created.")
 
-def main():
+
+def main(): 
     if os.path.exists(TEMPLATE_PROJECT_DIR):
         start_new_project()
     else :
-        print(f"Project template file({TEMPLATE_PROJECT_NAME}) does not found.\nCreating project template file at {TEMPLATE_PROJECT_DIR}...")
-        os.makedirs(f"{TEMPLATE_PROJECT_DIR}",exist_ok=True)
-        program_file = pathlib.Path(f"{TEMPLATE_PROJECT_DIR}/{TEMPLATE_PROJECT_NAME}.pde")
-        program_file.touch()
-        print(f"Project template file created.")
+        create_template()# テンプレートがなければ作成する。
         start_new_project()
 
     input("\nPress 'Enter' to stop this command.");
-
 
 if __name__ == "__main__":
     main()
